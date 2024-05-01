2ab3a25be34e91e09e719e3d1cdfde3aa9c686c2
jaewook <wodnr5050@gmail.com>
2024-05-01 09:54:15 +0900

    first commit

---
+++ sql/prj1.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE prj1;
+
+USE prj1;
+
+CREATE TABLE board
+(
+    id       INT PRIMARY KEY AUTO_INCREMENT,
+    title    VARCHAR(200)  NOT NULL,
+    content  VARCHAR(3000) NOT NULL,
+    writer   VARCHAR(100)  NOT NULL,
+    inserted DATETIME      NOT NULL DEFAULT NOW()
+);
